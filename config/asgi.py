bace3000d33e8d456a5d543979e5ab5af21cd66e
Preetam Swami <preetam@Preetams-MacBook-Pro.local>
2019-10-14 18:23:49 +0530

    asgi production

---
+++ config/asgi.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import django
+from channels.routing import get_default_application
+
+# This allows easy placement of apps within the interior
+# digital_saarthi directory.
+app_path = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+)
+sys.path.append(os.path.join(app_path, "digital_saarthi"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+django.setup()
+application = get_default_application()
